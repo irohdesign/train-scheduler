dd3b608d9a57d1cd8806952938d43698b1002f66
Jalia Evans <irohdesign@outlook.com>
2019-02-04 20:49:12 -0500

    working on train scheduling app

---
+++ server.js
@@ -0,0 +1,17 @@
+// dependencies 
+
+
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDlQGqneV91nf2hPoMh68A2xCjb2JFgqw4",
+authDomain: "train-schedule-app-454e1.firebaseapp.com",
+databaseURL: "https://train-schedule-app-454e1.firebaseio.com",
+projectId: "train-schedule-app-454e1",
+storageBucket: "train-schedule-app-454e1.appspot.com",
+messagingSenderId: "854438419641"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
